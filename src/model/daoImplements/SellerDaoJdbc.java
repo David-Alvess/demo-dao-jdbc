213defd3678d6ae0a54db65972c2740c6b5516e3
David Alves <davidalvess1996@gmail.com>
2023-04-14 12:06:10 -0300

    SellerDaoJdbc and DaoFactory

---
+++ src/model/daoImplements/SellerDaoJdbc.java
@@ -0,0 +1,33 @@
+package model.daoImplements;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJdbc implements SellerDao {
+    @Override
+    public void insert(Seller seller) {
+
+    }
+
+    @Override
+    public void update(Seller seller) {
+
+    }
+
+    @Override
+    public void deleteByID(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
